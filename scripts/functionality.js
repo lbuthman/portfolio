954fb4a4e552a66ae0aeb060662ea1218f82da16
lbuthman <lbuthman@gmail.com>
2017-04-30 19:43:01 -0500

    Init commit for script file

---
+++ scripts/functionality.js
@@ -0,0 +1,8 @@
+/**
+ * Created by lbuthman on 4/30/17.
+ */
+
+/*Use Bootstrap to implement Modal Box and Slide show
+* https://www.w3schools.com/howto/howto_js_slideshow.asp
+* https://www.w3schools.com/bootstrap/bootstrap_images.asp
+* */
