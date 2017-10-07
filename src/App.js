1126b75a709ac7b20d3da5dfd13aa5d1f1277ed2
lbuthman <lbuthman@gmail.com>
2017-10-07 08:07:59 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="red container hello">
+        <header>
+          <h1 className="white-text">Welcome to React</h1>
+        </header>
+        <p className="orange">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
