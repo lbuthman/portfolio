97e148d00cea2363f36f6c3741eb8714cffe8ee5
lbuthman <lbuthman@gmail.com>
2017-10-08 19:55:38 -0500

    feat: Add my variation for top navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import $ from 'jquery';
+
+class TopNav extends Component {
+
+  componentDidMount() {
+    $(document).ready(function(){
+      $('ul.tabs').tabs();
+    });
+  }
+
+  render() {
+    return(
+      <div className="row">
+        <div className="col s12">
+          <ul className="tabs">
+            <li className="tab col s3"><a className="active" href="#projects">Projects</a></li>
+            <li className="tab col s3"><a href="#experience">Experience</a></li>
+            <li className="tab col s3"><a href="#education">Education</a></li>
+            <li className="tab col s3"><a href="#resume">Resume</a></li>
+            <li className="tab col s3"><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        <div id="projects" className="col s12">Here are a bunch of projects I did.</div>
+        <div id="education" className="col s12">Here is info about my education.</div>
+        <div id="resume" className="col s12">Here is a beautiful resume you can view/print.</div>
+        <div id="contact" className="col s12">Here is how to contact me.</div>
+        <div id="experience" className="col s12">Here is my experience.</div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
 
   render() {
     return (
-      <div className="red container hello">
-        <header>
-          <h1 className="white-text">Welcome to React</h1>
-        </header>
-        <p className="orange">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <TopNav/>
     );
   }
 }
