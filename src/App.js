d29776111d926b6ffee72390be7f4627c5fe7685
lbuthman <lbuthman@gmail.com>
2017-10-09 18:49:26 -0500

    reorder tabs and add fixed-width class

--- src/App.js
+++ src/App.js
@@ -27,19 +27,19 @@ class TopNav extends Component {
     return(
       <div className="row">
         <div className="col s12">
-          <ul className="tabs">
+          <ul className="tabs tabs-fixed-width">
             <li className="tab col s3"><a className="active" href="#projects">Projects</a></li>
             <li className="tab col s3"><a href="#experience">Experience</a></li>
-            <li className="tab col s3"><a href="#education">Education</a></li>
             <li className="tab col s3"><a href="#resume">Resume</a></li>
+            <li className="tab col s3"><a href="#education">Education</a></li>
             <li className="tab col s3"><a href="#contact">Contact</a></li>
           </ul>
         </div>
         <div id="projects" className="col s12">Here are a bunch of projects I did.</div>
-        <div id="education" className="col s12">Here is info about my education.</div>
-        <div id="resume" className="col s12">Here is a beautiful resume you can view/print.</div>
-        <div id="contact" className="col s12">Here is how to contact me.</div>
         <div id="experience" className="col s12">Here is my experience.</div>
+        <div id="resume" className="col s12">Here is a beautiful resume you can view/print.</div>
+        <div id="education" className="col s12">Here is info about my education.</div>
+        <div id="contact" className="col s12">Here is how to contact me.</div>
       </div>
     );
   }
